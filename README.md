5367822c9279b80d7cd2d832661ab4c8ce21c10d
Luukas Pörtfors <lajp@lajp.fi>
2022-03-09 12:20:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# testaustime-r
+
+Visualize your testaustime data with cool graphs generated with an R script using ggplot2
+
+## How to use?
+1. Install necessary packages (and R if you dont have it):
+    ```R
+    install.packages("httr", "rjson", "ggplot2", "ggthemes")
+    ```
+2. Run the script (first insert your token into it):
+    ```sh
+    Rscript plot.R
+    ```
+3. Look at cool images
+
+## How does it look?
+(please ignore the awful colorscheme, I'm working on it (not))
+![](./demo/language_and_duration_by_date.png)
+![](./demo/project_and_duration_by_date.png)
---
+++ plot.R
@@ -0,0 +1,39 @@
+qry <- "https://time.lajp.fi/activity/data?min_duration=1"
+bearer <- "Bearer <YOURTOKEN>"
+
+library("httr")
+res <- GET(qry, add_headers(Authorization=bearer))
+
+library("rjson")
+result <- fromJSON(content(res,as="text"))
+
+result <- lapply(result, function(x) {
+  x[sapply(x, is.null)] <- NA
+  unlist(x)
+})
+
+df <- as.data.frame(do.call("rbind", result))
+
+df$duration <- strtoi(df$duration)
+df$start_time <- as.Date(df$start_time)
+
+data <- aggregate(duration~language+project_name+start_time, data=df, FUN=sum, na.rm=TRUE)
+print(data)
+
+library("ggplot2")
+library("ggthemes")
+
+png(file="language_and_duration_by_date.png", width=1024, height=720)
+par(bg="#1c1c1c")
+
+ggplot(data, aes(fill=language, y=duration, x=start_time)) + geom_bar(position="stack", stat="identity") + theme_dark() +
+    theme(plot.background = element_rect(fill = "#3c3835", color = "black"), panel.background = element_rect("#242221", color="black")) +
+    labs(x="Date", y="Time coded") + theme(text=element_text(size=20), legend.background = element_rect("gray", color="black"))
+
+png(file="project_and_duration_by_date.png", width=1024, height=720)
+ggplot(data, aes(fill=project_name, y=duration, x=start_time)) + geom_bar(position="stack", stat="identity") + theme_dark() +
+    theme(plot.background = element_rect(fill = "#3c3835", color = "black"), panel.background = element_rect("#242221", color="black")) +
+    labs(x="Date", y="Time coded") + theme(text=element_text(size=20), legend.background = element_rect("gray", color="black"))
+
+while (!is.null(dev.list()))
+    dev.off()
