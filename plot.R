554077614832633c5b5e1477ab77e8f2109412e8
Luukas Pörtfors <lajp@iki.fi>
2022-03-10 17:01:26 +0200

    Make graphs handsomer

--- plot.R
+++ plot.R
@@ -25,16 +25,24 @@ library("ggthemes")
 
 png(file="language_and_duration_by_date.png", width=1024, height=720)
 
+duration_by_language <- aggregate(duration~language, data=data, FUN=sum)
+duration_by_language <- duration_by_language[order(duration_by_language$duration, decreasing=T), ]
+
 ggplot(data, aes(fill=language, y=duration, x=start_time)) + geom_bar(position="stack", stat="identity") + theme_dark() +
     theme(plot.background = element_rect(fill = "#3c3835", color = "black"), panel.background = element_rect("#242221", color="black")) +
     labs(x="Date", y="Time coded") + theme(text=element_text(size=20, color="#fbf1c7"), legend.background = element_rect("#4c4541", color="black")) +
-    theme(axis.text=element_text(color="#b8bb26")) + theme(plot.title=element_text(color="#fb4934"))
+    theme(axis.text=element_text(color="#b8bb26")) + theme(plot.title=element_text(color="#fb4934")) +
+    scale_fill_discrete(breaks=duration_by_language$language, labels=paste(duration_by_language$language, ":", duration_by_language$duration, "s"))
+
+duration_by_project_name <- aggregate(duration~project_name, data=data, FUN=sum)
+duration_by_project_name <- duration_by_project_name[order(duration_by_project_name$duration, decreasing=T), ]
 
 png(file="project_and_duration_by_date.png", width=1024, height=720)
 ggplot(data, aes(fill=project_name, y=duration, x=start_time)) + geom_bar(position="stack", stat="identity") + theme_dark() +
     theme(plot.background = element_rect(fill = "#3c3835", color = "black"), panel.background = element_rect("#242221", color="black")) +
     labs(x="Date", y="Time coded") + theme(text=element_text(size=20, color="#fbf1c7"), legend.background = element_rect("#4c4541", color="black")) +
-    theme(axis.text=element_text(color="#b8bb26")) + theme(plot.title=element_text(color="#fb4934"))
+    theme(axis.text=element_text(color="#b8bb26")) + theme(plot.title=element_text(color="#fb4934")) +
+    scale_fill_discrete(breaks=duration_by_project_name$project_name, labels=paste(duration_by_project_name$project_name, ":", duration_by_project_name$duration, "s"))
 
 while (!is.null(dev.list()))
     dev.off()
